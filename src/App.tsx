4b5a04c53d582f746ef381bf2f16b43f1ccbfb00
[dyad] <github@sollux.tech>
2025-10-07 15:20:25 -0300

    Reverted all changes back to version 791d25043c1a787ccae7921e2a79cc1d9a0be8a2

--- src/App.tsx
+++ src/App.tsx
@@ -1,51 +1,27 @@
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
-import Layout from './components/Layout';
-import Index from './pages/Index';
-import Login from './pages/Login';
-import { SessionContextProvider } from './components/SessionContextProvider';
-import { Toaster } from 'sonner';
-import IdPage from './pages/IdPage';
-import CompanyManagementPage from './pages/CompanyManagementPage';
-import { CompanyProvider } from './components/CompanyContext';
-import PulsePage from './pages/PulsePage'; 
-import UserManagementPage from './pages/UserManagementPage'; // Importando a nova página
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { Toaster } from "@/components/ui/toaster";
+import { Toaster as Sonner } from "@/components/ui/sonner";
+import { TooltipProvider } from "@/components/ui/tooltip";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Index from "./pages/Index";
+import NotFound from "./pages/NotFound";
 
 const queryClient = new QueryClient();
 
-function App() {
-  return (
-    <Router>
-      <QueryClientProvider client={queryClient}>
-        <SessionContextProvider>
-          <CompanyProvider>
-            <Toaster />
-            <Routes>
-              <Route path="/login" element={<Login />} />
-              <Route
-                path="*"
-                element={
-                  <Layout>
-                    <Routes>
-                      <Route path="/" element={<Navigate to="/pulse" replace />} /> 
-                      <Route path="/pulse" element={<PulsePage />} /> 
-                      <Route path="/id" element={<IdPage />} />
-                      <Route path="/id/companies" element={<CompanyManagementPage />} />
-                      <Route path="/id/users" element={<UserManagementPage />} /> {/* Nova rota */}
-                      {/* Adicione outras rotas aqui */}
-                      <Route path="/ops" element={<Index />} /> 
-                      <Route path="/connect" element={<Index />} />
-                      <Route path="/core" element={<Index />} />
-                    </Routes>
-                  </Layout>
-                }
-              />
-            </Routes>
-          </CompanyProvider>
-        </SessionContextProvider>
-      </QueryClientProvider>
-    </Router>
-  );
-}
+const App = () => (
+  <QueryClientProvider client={queryClient}>
+    <TooltipProvider>
+      <Toaster />
+      <Sonner />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Index />} />
+          {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </BrowserRouter>
+    </TooltipProvider>
+  </QueryClientProvider>
+);
 
 export default App;
