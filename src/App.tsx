c463c2e873efe1498b5cc6e3d06fe56f48568caa
[dyad] <github@sollux.tech>
2025-10-07 14:33:33 -0300

    [dyad] Adicionado QueryClientProvider para TanStack Query - wrote 1 file(s)

--- src/App.tsx
+++ src/App.tsx
@@ -8,35 +8,40 @@ import IdPage from './pages/IdPage';
 import CompanyManagementPage from './pages/CompanyManagementPage';
 import { CompanyProvider } from './components/CompanyContext';
 import PulsePage from './pages/PulsePage'; 
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'; // Importando QueryClient e QueryClientProvider
+
+const queryClient = new QueryClient(); // Criando uma nova instância do QueryClient
 
 function App() {
   return (
-    <Router> {/* O Router deve ser o componente pai para useNavigate funcionar */}
-      <SessionContextProvider>
-        <CompanyProvider>
-          <Toaster />
-          <Routes>
-            <Route path="/login" element={<Login />} />
-            <Route
-              path="*"
-              element={
-                <Layout>
-                  <Routes>
-                    <Route path="/" element={<Navigate to="/pulse" replace />} /> 
-                    <Route path="/pulse" element={<PulsePage />} /> 
-                    <Route path="/id" element={<IdPage />} />
-                    <Route path="/id/companies" element={<CompanyManagementPage />} />
-                    {/* Adicione outras rotas aqui */}
-                    <Route path="/ops" element={<Index />} /> 
-                    <Route path="/connect" element={<Index />} />
-                    <Route path="/core" element={<Index />} />
-                  </Routes>
-                </Layout>
-              }
-            />
-          </Routes>
-        </CompanyProvider>
-      </SessionContextProvider>
+    <Router>
+      <QueryClientProvider client={queryClient}> {/* Envolvendo a aplicação com QueryClientProvider */}
+        <SessionContextProvider>
+          <CompanyProvider>
+            <Toaster />
+            <Routes>
+              <Route path="/login" element={<Login />} />
+              <Route
+                path="*"
+                element={
+                  <Layout>
+                    <Routes>
+                      <Route path="/" element={<Navigate to="/pulse" replace />} /> 
+                      <Route path="/pulse" element={<PulsePage />} /> 
+                      <Route path="/id" element={<IdPage />} />
+                      <Route path="/id/companies" element={<CompanyManagementPage />} />
+                      {/* Adicione outras rotas aqui */}
+                      <Route path="/ops" element={<Index />} /> 
+                      <Route path="/connect" element={<Index />} />
+                      <Route path="/core" element={<Index />} />
+                    </Routes>
+                  </Layout>
+                }
+              />
+            </Routes>
+          </CompanyProvider>
+        </SessionContextProvider>
+      </QueryClientProvider>
     </Router>
   );
 }
